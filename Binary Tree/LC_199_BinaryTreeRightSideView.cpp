423cc9fe9dc6cedefc4b97f5befddffd628df01b
Anmol Mittal <mittalanmol0309@gmail.com>
2026-08-26 23:25:37 +0530

    right side view by using traversal

---
+++ Binary Tree/LC_199_BinaryTreeRightSideView.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+//  TC -> O(N) , SC-> O(N) , N-> no of nodes in the tree
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root) return {};
+        vector<int>ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            vector<int>temp;
+            while(sz--){
+                auto node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(temp.back());
+        }
+        return ans;
+    }
+};
