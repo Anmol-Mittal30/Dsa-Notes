59d494023b4f57d34ee35eb1a21ae5e138aa1d5c
Anmol Mittal <mittalanmol0309@gmail.com>
2026-09-05 20:14:09 +0530

    update to constant space

--- DP/1D Dp/LC_198-house-robber.cpp
+++ DP/1D Dp/LC_198-house-robber.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    // TC -> O(n) , SC-> O(n)
+     // TC -> O(n) , SC-> O(n)
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n <= 1) return nums[n-1];
+    //     vector<int>dp(n);
+    //     dp[0] = nums[0];
+    //     dp[1] = max(nums[0] , nums[1]);
+    //     for(int i = 2; i < n ; i++){
+    //         dp[i] = max(dp[i-1] , nums[i] + dp[i-2]);
+    //     }
+    //     return max(dp[n-1] , dp[n-2]);
+    // }
+
+
+    // Approach 2 constant space
+    // TC -> O(n) , SC-> O(1)
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n <= 1) return nums[n-1];
-        vector<int>dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0] , nums[1]);
+        int a = nums[0];
+        int b = max(nums[0] , nums[1]);
         for(int i = 2; i < n ; i++){
-            dp[i] = max(dp[i-1] , nums[i] + dp[i-2]);
+            int c  = max(b , nums[i] + a);
+            a = b;
+            b = c;
         }
-        return max(dp[n-1] , dp[n-2]);
+        return b ;
     }
 };
+
